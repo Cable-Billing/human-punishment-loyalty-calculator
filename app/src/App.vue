9f0b3f2ee030fa44041ff4c293973bfd7a33b822
Caleb Billing <billing.caleb14@gmail.com>
2024-06-13 23:08:44 +0930

    Init app

---
+++ app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainCalculator from "@/components/MainCalculator.vue";
+</script>
+
+<template>
+  <v-container>
+    <MainCalculator />
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/MainCalculator.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <v-col>
+        <h1>Human Punishment Loyalty Calculator</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <LoyaltyCard @selection-changed="leftLoyaltyChanged" />
+      </v-col>
+      <v-col>
+        <IdentityCard @selection-changed="identityChanged" />
+      </v-col>
+      <v-col>
+        <LoyaltyCard @selection-changed="rightLoyaltyChanged" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import IdentityCard from "@/components/IdentityCard.vue";
+import LoyaltyCard from "@/components/LoyaltyCard.vue";
+
+export default {
+  name: "MainCalculator",
+  components: {LoyaltyCard, IdentityCard},
+  methods: {
+    identityChanged(params) {
+      console.log(params);
+    },
+    leftLoyaltyChanged(params) {
+      console.log(params);
+    },
+    rightLoyaltyChanged(params) {
+      console.log(params);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
