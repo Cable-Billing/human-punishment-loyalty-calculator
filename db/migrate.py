ec6220fee292dba64cd7fa9d651e6f17aea4989e
Caleb Billing <billing.caleb14@gmail.com>
2024-06-19 21:42:17 +0930

    #7 Started implementing migrate to run the migrations

--- db/migrate.py
+++ db/migrate.py
@@ -1,9 +1,8 @@
 import psycopg2
+from os import listdir
 
 
-def migration_initialise():
-    connection = psycopg2.connect(user="root", password="root", host="postgres-db", port="5432", database="hp_data")
-
+def migration_initialise(connection):
     cursor = connection.cursor()
     print('Checking if \'migration_history\' tables exists...')
     cursor.execute("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'migration_history');")
@@ -13,20 +12,33 @@ def migration_initialise():
         print('\'migration_history\' table exists, nothing to do')
     else:
         print('\'migration_history\' table does not exist, creating...')
-        cursor.execute("CREATE TABLE migration_history (migration_number CHARACTER(12), migrated_timestamp TIMESTAMP WITH TIME ZONE);")
+        cursor.execute('CREATE TABLE migration_history (migration_number CHARACTER(12), migrated_timestamp TIMESTAMP WITH TIME ZONE);')
         connection.commit()
         print('\'migration_history\' table created')
 
-    connection.close()
+    cursor.close()
 
 
-def migrate():
-    print('test')
+def migrate(connection):
+    cursor = connection.cursor()
+    migration_files = listdir('./migrations')
+
+    for file_name in migration_files:
+        migration_number = file_name[:12]
+        cursor.execute("SELECT COUNT(*) FROM migration_history WHERE migration_number = %(migration_number)s", {'migration_number': migration_number})
+        row = cursor.fetchone()
+
+        if row and row[0] == 0:
+            print()
+
+    cursor.close()
 
 
 def main():
-    migration_initialise()
-    migrate()
+    connection = psycopg2.connect(user='root', password='root', host='postgres-db', port='5432', database='hp_data')
+    migration_initialise(connection)
+    migrate(connection)
+    connection.close()
 
 
 if __name__ == '__main__':
