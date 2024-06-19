dcadec58fad234ab4b224011ff9000f370afcef5
Caleb Billing <billing.caleb14@gmail.com>
2024-06-19 21:17:40 +0930

    #7 Decided to merge initialise.py and migrate.py together

--- db/migrate.py
+++ db/migrate.py
@@ -1,25 +1,32 @@
 import psycopg2
 
 
-def main():
-    print('Initialising hp_data Database')
+def migration_initialise():
     connection = psycopg2.connect(user="root", password="root", host="postgres-db", port="5432", database="hp_data")
-    print('Connection opened')
 
     cursor = connection.cursor()
+    print('Checking if \'migration_history\' tables exists...')
     cursor.execute("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'migration_history');")
     row = cursor.fetchone()
 
     if row and row[0]:
-        print('Migration history table exists, nothing to do')
+        print('\'migration_history\' table exists, nothing to do')
     else:
-        print('Migration History table does not exist, creating...')
+        print('\'migration_history\' table does not exist, creating...')
         cursor.execute("CREATE TABLE migration_history (migration_number INT, migrated_timestamp TIMESTAMP WITH TIME ZONE);")
         connection.commit()
-        print('Migration History table created')
+        print('\'migration_history\' table created')
 
     connection.close()
-    print("Connection closed")
+
+
+def migrate():
+    print('test')
+
+
+def main():
+    migration_initialise()
+    migrate()
 
 
 if __name__ == '__main__':
