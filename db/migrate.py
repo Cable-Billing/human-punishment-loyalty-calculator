6f0490062302ac6c2d85fce60845c6519702d3d2
Caleb Billing <billing.caleb14@gmail.com>
2024-07-05 22:16:13 +0930

    Added ';' to SQL because Postgres likes that

--- db/migrate.py
+++ db/migrate.py
@@ -3,12 +3,12 @@ from os import listdir
 
 
 def migration_initialise(connection):
-    row = connection.run("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'migration_history')")[0]
+    row = connection.run("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'migration_history');")[0]
 
     if row[0]:
         print('Table \'migration_history\': exists')
     else:
-        connection.run('CREATE TABLE migration_history (migration_number CHARACTER(12), migrated_timestamp TIMESTAMP WITH TIME ZONE)')
+        connection.run('CREATE TABLE migration_history (migration_number CHARACTER(12), migrated_timestamp TIMESTAMP WITH TIME ZONE);')
         print('Table \'migration_history\': created')
 
 
@@ -17,14 +17,14 @@ def migrate(connection):
 
     for file_name in migration_files:
         migration_number = file_name[:12]
-        row = connection.run("SELECT EXISTS (SELECT FROM migration_history WHERE migration_number = :migration_number)", migration_number=migration_number)[0]
+        row = connection.run("SELECT EXISTS (SELECT FROM migration_history WHERE migration_number = :migration_number);", migration_number=migration_number)[0]
 
         if row[0]:
             print(f'Migration \'{migration_number}\': already run')
         else:
             file = open(f'./migrations/{file_name}')
             connection.run(file.read())
-            connection.run("INSERT INTO migration_history(migration_number, migrated_timestamp) VALUES (:migration_number, NOW())", migration_number=migration_number)
+            connection.run("INSERT INTO migration_history(migration_number, migrated_timestamp) VALUES (:migration_number, NOW());", migration_number=migration_number)
             print(f'Migration \'{migration_number}\': ran')
 
 
